19dad5b401f215a806dce7737acf6bf303f4e97b
mdraperext <michael.draper@extend.com>
2021-04-07 20:21:28 -0500

    remove style changes

--- view/frontend/web/js/warranties.js
+++ view/frontend/web/js/warranties.js
@@ -4,24 +4,24 @@ define([
 ], function ($, _) {
 
     return function (params) {
-        /*VK compatibility
+        
         Extend.buttons.render('#extend-offer', {
             referenceId: params.productSku
-        });*/
-        Extend.buttons.render('#extend-offer', {referenceId: params.productSku}, function(){
-            //select extend iframe
-            var iframe = document.querySelector('#extend-offer iframe')
-            //if we have an iframe we will select that iframes document
-            var extendDocument = iframe ? iframe.contentWindow.document : null;
-            //if we have a document, we will select all of the buttons, and then we can style those buttons accordingly
-            var offerBtns = extendDocument ? extendDocument.querySelectorAll('.btn-offer') : null;
-            if(offerBtns){
-              offerBtns.forEach(function(btn) {
-                //if you are overriding any existing styling be sure to use !important
-                btn.style = "padding: 0.3rem !important"
-              })
-            }
         });
+        // Extend.buttons.render('#extend-offer', {referenceId: params.productSku}, function(){
+        //     //select extend iframe
+        //     var iframe = document.querySelector('#extend-offer iframe')
+        //     //if we have an iframe we will select that iframes document
+        //     var extendDocument = iframe ? iframe.contentWindow.document : null;
+        //     //if we have a document, we will select all of the buttons, and then we can style those buttons accordingly
+        //     var offerBtns = extendDocument ? extendDocument.querySelectorAll('.btn-offer') : null;
+        //     if(offerBtns){
+        //       offerBtns.forEach(function(btn) {
+        //         //if you are overriding any existing styling be sure to use !important
+        //         btn.style = "padding: 0.3rem !important"
+        //       })
+        //     }
+        // });
 
         $(document).ready(function () {
             $('div.product-options-wrapper').on('change',() => {
