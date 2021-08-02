0d63f45239fbd005fd390d3428b2a199e9c89d8f
Andrew Hofstetter <ahofs@guidance.com>
2021-08-02 14:43:41 -0700

    EX-17 Refactor API plugin for orders

--- Plugin/Api/OrderItemRepositoryInterfacePlugin.php
+++ Plugin/Api/OrderItemRepositoryInterfacePlugin.php
@@ -7,46 +7,37 @@
  * @package     Warranty
  * @copyright   Copyright (c) 2021 Extend Inc. (https://www.extend.com/)
  */
-
-declare(strict_types=1);
-
 namespace Extend\Warranty\Plugin\Api;
-
-use Magento\Sales\Api\OrderItemRepositoryInterface;
-use Magento\Sales\Api\Data\OrderItemExtensionFactory;
-use Magento\Sales\Api\Data\OrderItemInterface;
-use Magento\Sales\Api\Data\OrderItemSearchResultInterface;
-use Magento\Framework\Serialize\Serializer\Json as JsonSerializer;
-use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Class OrderItemRepositoryInterfacePlugin
+ * @package Extend\Warranty\Plugin\Api
  */
 class OrderItemRepositoryInterfacePlugin
 {
     /**
      * Extension attributes
      */
-    const CONTRACT_ID = 'contract_id';
-    const PRODUCT_OPTIONS = 'product_options';
-    const WARRANTY_ID = 'warranty_id';
+    const CONTRACT_ID        = 'contract_id';
+    const PRODUCT_OPTIONS    = 'product_options';
+    const WARRANTY_ID        = 'warranty_id';
     const ASSOCIATED_PRODUCT = 'associated_product';
-    const REFUND = 'refund';
-    const WARRANTY_TERM = 'warranty_term';
+    const REFUND             = 'refund';
+    const WARRANTY_TERM      = 'warranty_term';
 
     /**
      * Order Extension Attributes Factory
      *
-     * @var OrderItemExtensionFactory
+     * @var \Magento\Sales\Api\Data\OrderItemExtensionFactory
      */
-    private $extensionFactory;
+    private $_extensionFactory;
 
     /**
      * Json Serializer
      *
-     * @var JsonSerializer
+     * @var \Magento\Framework\Serialize\Serializer\Json
      */
-    private $jsonSerializer;
+    private $_jsonSerializer;
 
     /**
      * List of product options
@@ -59,47 +50,57 @@ class OrderItemRepositoryInterfacePlugin
     ];
 
     /**
-     * OrderItemRepositoryInterfacePlugin constructor
-     *
-     * @param OrderItemExtensionFactory $extensionFactory
-     * @param JsonSerializer $jsonSerializer
+     * OrderItemRepositoryInterfacePlugin constructor.
+     * @param \Magento\Sales\Api\Data\OrderItemExtensionFactory $extensionFactory
+     * @param \Magento\Framework\Serialize\Serializer\Json $jsonSerializer
      */
     public function __construct(
-        OrderItemExtensionFactory $extensionFactory,
-        JsonSerializer $jsonSerializer
-    ) {
-        $this->extensionFactory = $extensionFactory;
-        $this->jsonSerializer = $jsonSerializer;
+        \Magento\Sales\Api\Data\OrderItemExtensionFactory $extensionFactory,
+        \Magento\Framework\Serialize\Serializer\Json $jsonSerializer
+    )
+    {
+        $this->_extensionFactory = $extensionFactory;
+        $this->_jsonSerializer = $jsonSerializer;
     }
 
     /**
      * Add "contract_id & product_options" extension attributes to order item data object to make it accessible in API data
      *
-     * @param OrderItemRepositoryInterface $subject
-     * @param OrderItemInterface $orderItem
-     * @return OrderItemInterface
+     * @param \Magento\Sales\Api\OrderItemRepositoryInterface $subject
+     * @param \Magento\Sales\Api\Data\OrderItemInterface $orderItem
+     * @return \Magento\Sales\Api\Data\OrderItemInterface
+     * @noinspection PhpUnusedParameterInspection
      */
     public function afterGet(
-        OrderItemRepositoryInterface $subject,
-        OrderItemInterface $orderItem
-    ): OrderItemInterface {
-        $contractId = $orderItem->getData(self::CONTRACT_ID);
-        $productOptions = $orderItem->getProductOptions();
-        $productOptionsJson = $this->getProductOptionsJson($orderItem, $productOptions);
+        \Magento\Sales\Api\OrderItemRepositoryInterface $subject,
+        \Magento\Sales\Api\Data\OrderItemInterface $orderItem
+    ): \Magento\Sales\Api\Data\OrderItemInterface
+    {
+        /** @var \Magento\Sales\Model\Order\Item $orderItem */
+        $contractId = (string)$orderItem->getData(self::CONTRACT_ID);
+        $productOptions = (array)$orderItem->getProductOptions();
+        $productOptionsJson = (string)$this->getProductOptionsJson($orderItem, $productOptions);
 
         foreach (self::PRODUCT_OPTION_LIST as $option) {
             $productOptions[$option] = $productOptions[$option] ?? null;
         }
 
         $extensionAttributes = $orderItem->getExtensionAttributes();
-        $extensionAttributes = $extensionAttributes ?: $this->extensionFactory->create();
+        /** @noinspection PhpUndefinedMethodInspection */
+        $extensionAttributes = $extensionAttributes ?: $this->_extensionFactory->create();
 
+        /** @noinspection PhpUndefinedMethodInspection */
         $extensionAttributes->setContractId($contractId);
+        /** @noinspection PhpUndefinedMethodInspection */
         $extensionAttributes->setProductOptions($productOptionsJson);
-        $extensionAttributes->setWarrantyId($productOptions[self::WARRANTY_ID]);
-        $extensionAttributes->setAssociatedProduct($productOptions[self::ASSOCIATED_PRODUCT]);
-        $extensionAttributes->setRefund($productOptions[self::REFUND]);
-        $extensionAttributes->setTerm($productOptions[self::WARRANTY_TERM]);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $extensionAttributes->setWarrantyId($productOptions[self::WARRANTY_ID] ?? '');
+        /** @noinspection PhpUndefinedMethodInspection */
+        $extensionAttributes->setAssociatedProduct($productOptions[self::ASSOCIATED_PRODUCT] ?? '');
+        /** @noinspection PhpUndefinedMethodInspection */
+        $extensionAttributes->setRefund((bool)$productOptions[self::REFUND]);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $extensionAttributes->setTerm($productOptions[self::WARRANTY_TERM] ?? '');
 
         $orderItem->setExtensionAttributes($extensionAttributes);
 
@@ -109,34 +110,44 @@ class OrderItemRepositoryInterfacePlugin
     /**
      * Add "contract_id & product_options" extension attributes to order item data object to make it accessible in API data
      *
-     * @param OrderItemRepositoryInterface $subject
-     * @param OrderItemSearchResultInterface $searchResult
-     * @return OrderItemSearchResultInterface
+     * @param \Magento\Sales\Api\OrderItemRepositoryInterface $subject
+     * @param \Magento\Sales\Api\Data\OrderItemSearchResultInterface $searchResult
+     * @return \Magento\Sales\Api\Data\OrderItemSearchResultInterface
+     * @noinspection PhpUnusedParameterInspection
      */
     public function afterGetList(
-        OrderItemRepositoryInterface $subject,
-        OrderItemSearchResultInterface $searchResult
-    ): OrderItemSearchResultInterface {
+        \Magento\Sales\Api\OrderItemRepositoryInterface $subject,
+        \Magento\Sales\Api\Data\OrderItemSearchResultInterface $searchResult
+    ): \Magento\Sales\Api\Data\OrderItemSearchResultInterface
+    {
         $ordersItems = $searchResult->getItems();
 
-        foreach ($ordersItems as &$orderItem) {
-            $contractId = $orderItem->getData(self::CONTRACT_ID);
-            $productOptions = $orderItem->getProductOptions();
-            $productOptionsJson = $this->getProductOptionsJson($orderItem, $productOptions);
+        foreach ($ordersItems as $orderItem) {
+            /** @var \Magento\Sales\Model\Order\Item $orderItem */
+            $contractId = (string)$orderItem->getData(self::CONTRACT_ID);
+            $productOptions = (array)$orderItem->getProductOptions();
+            $productOptionsJson = (string)$this->getProductOptionsJson($orderItem, $productOptions);
 
             foreach (self::PRODUCT_OPTION_LIST as $option) {
                 $productOptions[$option] = $productOptions[$option] ?? null;
             }
 
             $extensionAttributes = $orderItem->getExtensionAttributes();
-            $extensionAttributes = $extensionAttributes ?: $this->extensionFactory->create();
+            /** @noinspection PhpUndefinedMethodInspection */
+            $extensionAttributes = $extensionAttributes ?: $this->_extensionFactory->create();
 
+            /** @noinspection PhpUndefinedMethodInspection */
             $extensionAttributes->setContractId($contractId);
+            /** @noinspection PhpUndefinedMethodInspection */
             $extensionAttributes->setProductOptions($productOptionsJson);
-            $extensionAttributes->setWarrantyId($productOptions[self::WARRANTY_ID]);
-            $extensionAttributes->setAssociatedProduct($productOptions[self::ASSOCIATED_PRODUCT]);
-            $extensionAttributes->setRefund($productOptions[self::REFUND]);
-            $extensionAttributes->setTerm($productOptions[self::WARRANTY_TERM]);
+            /** @noinspection PhpUndefinedMethodInspection */
+            $extensionAttributes->setWarrantyId($productOptions[self::WARRANTY_ID] ?? '');
+            /** @noinspection PhpUndefinedMethodInspection */
+            $extensionAttributes->setAssociatedProduct($productOptions[self::ASSOCIATED_PRODUCT] ?? '');
+            /** @noinspection PhpUndefinedMethodInspection */
+            $extensionAttributes->setRefund((bool)$productOptions[self::REFUND]);
+            /** @noinspection PhpUndefinedMethodInspection */
+            $extensionAttributes->setTerm($productOptions[self::WARRANTY_TERM] ?? '');
 
             $orderItem->setExtensionAttributes($extensionAttributes);
         }
@@ -147,18 +158,23 @@ class OrderItemRepositoryInterfacePlugin
     /**
      * Get product options JSON
      *
-     * @param OrderItemInterface $orderItem
+     * @param \Magento\Sales\Api\Data\OrderItemInterface $orderItem
      * @param array $productOptions
      * @return string
+     * @noinspection PhpUnusedLocalVariableInspection
      */
-    private function getProductOptionsJson(OrderItemInterface $orderItem, array $productOptions): string
+    private function getProductOptionsJson(
+        \Magento\Sales\Api\Data\OrderItemInterface $orderItem,
+        array $productOptions
+    ): string
     {
+        /** @var \Magento\Sales\Model\Order\Item $orderItem */
         try {
             $productOptionsJson = $orderItem->getData(self::PRODUCT_OPTIONS);
             if (!is_string($productOptionsJson)) {
-                $productOptionsJson = $this->jsonSerializer->serialize($productOptions);
+                $productOptionsJson = $this->_jsonSerializer->serialize($productOptions);
             }
-        } catch (LocalizedException $exception) {
+        } catch (\Exception $e) {
             $productOptionsJson = '';
         }
 
