a245d8ebe7e2a1997349679cc5941c46fa66510c
AHoncharenko <sagaydakam@gmail.com>
2022-01-27 00:18:50 +0200

    [EX-218] [M2] Extension compatibility improvements: code clean-up

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,8 +5,6 @@ var config = {
             productWarrantyOffers: 'Extend_Warranty/js/product/product-warranty-offers',
             cartWarrantyOffers: 'Extend_Warranty/js/cart/cart-warranty-offers',
 
-            extendWarranties: 'Extend_Warranty/js/warranties',
-            cartAddWarranty: 'Extend_Warranty/js/cart/addToCart',
             'Magento_Catalog/template/product/image_with_borders.html':
                 'Extend_Warranty/template/product/image_with_borders.html'
         }
