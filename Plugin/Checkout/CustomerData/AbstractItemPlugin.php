7706b9314c7389d9f98af428046aa5eeed8e0f02
Mikhail Shelupets <w8miracle@gmail.com>
2022-02-04 19:27:58 +0200

    minicart warranty

--- Plugin/Checkout/CustomerData/AbstractItemPlugin.php
+++ Plugin/Checkout/CustomerData/AbstractItemPlugin.php
@@ -12,14 +12,33 @@ declare(strict_types=1);
 
 namespace Extend\Warranty\Plugin\Checkout\CustomerData;
 
+use Magento\Framework\UrlInterface;
 use Magento\Checkout\CustomerData\AbstractItem;
 use Extend\Warranty\Model\Product\Type;
+use Extend\Warranty\Helper\Api\Data as DataHelper;
+use Extend\Warranty\Helper\Tracking as TrackingHelper;
 
 /**
  * Class AbstractItemPlugin
  */
 class AbstractItemPlugin
 {
+    private $urlBuilder;
+
+    private $dataHelper;
+
+    private $trackingHelper;
+
+    public function __construct(
+        UrlInterface $urlBuilder,
+        DataHelper $dataHelper,
+        TrackingHelper $trackingHelper
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->dataHelper = $dataHelper;
+        $this->trackingHelper = $trackingHelper;
+    }
+
     /**
      * Set 'isWarranty' flag for product image
      *
@@ -27,10 +46,24 @@ class AbstractItemPlugin
      * @param array $result
      * @return array
      */
-    public function afterGetItemData(AbstractItem $subject, array $result): array
+    public function afterGetItemData(AbstractItem $subject, array $result, $item): array
     {
+        $test = $item;
         $result['product_image']['isWarranty'] = isset($result['product_type']) && $result['product_type'] === Type::TYPE_CODE;
+        $result['is_can_add_warranty'] = true;
+        $result['warranty_add_url'] = $this->getWarrantyAddUrl();
+        $result['product_parent_id'] = $this->getParentId($item);
 
         return $result;
+    }
+
+    private function getWarrantyAddUrl()
+    {
+        return $this->urlBuilder->getUrl('warranty/cart/add');
+    }
+
+    private function getParentId($item)
+    {
+        return $item->getOptionByCode('simple_product') ? $item->getProductId() : '';
     }
 }
--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,6 +6,13 @@ var config = {
             cartAddLeadWarranty : 'Extend_Warranty/js/cart/addLead',
             'Magento_Catalog/template/product/image_with_borders.html':
                 'Extend_Warranty/template/product/image_with_borders.html'
+        },
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/minicart': {
+                'Extend_Warranty/js/view/minicart_mixin': true
+            }
         }
     }
 };
