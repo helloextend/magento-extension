fd015cdb11374ed162016b8c2ba9b8a83768aafc
AHoncharenko <sagaydakam@gmail.com>
2022-02-08 02:01:57 +0200

    [EX-218] [M2] Extension compatibility improvements: grouped product view page

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,6 +4,7 @@ var config = {
             extendWarrantyOffers: 'Extend_Warranty/js/warranty-offers-base',
             simpleProductWarranty: 'Extend_Warranty/js/product/simple-warranty',
             configurableProductWarranty: 'Extend_Warranty/js/product/configurable-warranty',
+            groupedProductWarranty: 'Extend_Warranty/js/product/grouped-warranty',
 
             cartItemWarranty: 'Extend_Warranty/js/cart/cart-item-warranty',
 			cartAddLeadWarranty : 'Extend_Warranty/js/cart/addLead',
