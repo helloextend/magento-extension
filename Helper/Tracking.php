18c97fc582d2538b77a7b112df1e7de081899b14
Mikhail Shelupets <mshelupets@blendium.com>
2022-02-18 16:32:44 +0200

    merge master branch.

--- Helper/Tracking.php
+++ Helper/Tracking.php
@@ -22,11 +22,6 @@ class Tracking extends \Magento\Framework\App\Helper\AbstractHelper
     const XML_PATH_TRACKING_ENABLED = 'warranty/tracking/enabled';
 
     /**
-     * @var Grouped
-     */
-    private $_grouped;
-
-    /**
      * @var \Magento\Customer\Model\Session
      */
     private $_customerSession;
@@ -34,16 +29,13 @@ class Tracking extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      * Tracking constructor.
      * @param \Magento\Framework\App\Helper\Context $context
-     * @param \Extend\Warranty\Helper\Grouped $groupedHelper
      * @param \Magento\Customer\Model\Session $customerSession
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Extend\Warranty\Helper\Grouped $groupedHelper,
         \Magento\Customer\Model\Session $customerSession
     )
     {
-        $this->_grouped = $groupedHelper;
         $this->_customerSession = $customerSession;
 
         parent::__construct(
@@ -138,9 +130,6 @@ class Tracking extends \Magento\Framework\App\Helper\AbstractHelper
     public function getWarrantyItemForQuoteItem(\Magento\Quote\Model\Quote\Item $quoteItem)
     {
         $sku = $quoteItem->getSku();
-        if ($quoteItem->getProductType() == \Magento\GroupedProduct\Model\Product\Type\Grouped::TYPE_CODE) {
-            $sku = $this->_grouped->getGroupedWarrantySku($quoteItem);
-        }
         /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $quoteItem->getQuote();
         foreach ($quote->getAllItems() as $item) {
