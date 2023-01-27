3de8f5f38493d6a85daad78bfa9b63ea99af6a37
Alexandra Honcharenko <sagaydakam@ukr.net>
2023-01-27 17:55:38 +0200

    [EX-434] [M2] create options to control the placement of offers on the PDP: using config in templates

--- ViewModel/Warranty.php
+++ ViewModel/Warranty.php
@@ -16,6 +16,7 @@ use Extend\Warranty\Model\WarrantyRelation;
 use Extend\Warranty\Helper\Tracking as TrackingHelper;
 use Extend\Warranty\Model\Api\Sync\Lead\LeadInfoRequest;
 use Extend\Warranty\Model\Offers as OfferModel;
+use Extend\Warranty\Model\Config\Source\ProductPagePlacement;
 use Magento\Backend\Model\Auth\Session as AdminSession;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Checkout\Model\Session as CheckoutSession;
@@ -243,6 +244,51 @@ class Warranty implements ArgumentInterface
     public function isProductDetailPageOffersEnabled(): bool
     {
         return $this->dataHelper->isProductDetailPageOffersEnabled();
+    }
+
+    /**
+     * Get PDP Offers Button placement (insertion point and logic)
+     *
+     * @param bool $isSimpleProduct
+     * @return array
+     */
+    public function getProductDetailPageOffersPlacement(bool $isSimpleProduct): array
+    {
+        $pdpDisplay = $this->dataHelper->getProductDetailPageOffersPlacement();
+        $placement = [
+            'insertionPoint' => null,
+            'insertionLogic' => null
+        ];
+
+        switch ($pdpDisplay) {
+            case ProductPagePlacement::ACTIONS_BEFORE:
+            case ProductPagePlacement::ACTIONS_AFTER:
+                $placement['insertionPoint'] = 'div.actions';
+                $placement['insertionLogic'] = $pdpDisplay === ProductPagePlacement::ACTIONS_BEFORE ? 'before' : 'after';
+                break;
+            case ProductPagePlacement::ADD_TO_CART_BEFORE:
+            case ProductPagePlacement::ADD_TO_CART_AFTER:
+                $placement['insertionPoint'] = 'button.tocart';
+                $placement['insertionLogic'] = $pdpDisplay === ProductPagePlacement::ADD_TO_CART_BEFORE ? 'before' : 'after';
+                break;
+            case ProductPagePlacement::QUANTITY_BEFORE:
+            case ProductPagePlacement::QUANTITY_AFTER:
+                $placement['insertionPoint'] = 'div.field.qty';
+                $placement['insertionLogic'] = $pdpDisplay === ProductPagePlacement::QUANTITY_BEFORE ? 'before' : 'after';
+                break;
+            case ProductPagePlacement::OPTIONS_BEFORE:
+            case ProductPagePlacement::OPTIONS_AFTER:
+                $placement['insertionPoint'] = $isSimpleProduct ? 'div.box-tocart' : 'div.product-options-wrapper';
+                $placement['insertionLogic'] = $pdpDisplay === ProductPagePlacement::OPTIONS_BEFORE ? 'before' : 'after';
+                break;
+            case ProductPagePlacement::SOCIAL_BEFORE:
+            case ProductPagePlacement::SOCIAL_AFTER:
+                $placement['insertionPoint'] = 'div.product-social-links';
+                $placement['insertionLogic'] = $pdpDisplay === ProductPagePlacement::SOCIAL_BEFORE ? 'before' : 'after';
+                break;
+        }
+
+        return $placement;
     }
 
     /**
