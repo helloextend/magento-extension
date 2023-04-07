9d46f88b6df32e9b2895379939f992fefda6f4a9
boorlyk <viktor@boorlyk.dp.ua>
2023-04-07 12:39:55 +0300

    [EX-474] [M2] Admin/Telesales Lead Token always showing, even after being consumed - fixed issue of showing consumed lead warranty offer - fixed issue for multistore case — used orderItem storeid to get correct config by orders scope

---
+++ Model/Api/Response/LeadInfoResponse.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Extend Warranty
+ *
+ * @author      Extend Magento Team <magento@guidance.com>
+ * @category    Extend
+ * @package     Warranty
+ * @copyright   Copyright (c) 2022 Extend Inc. (https://www.extend.com/)
+ */
+
+namespace Extend\Warranty\Model\Api\Response;
+
+class LeadInfoResponse
+{
+    /** @var int|null */
+    protected $expirationDate;
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    const STATUS_LIVE = 'live';
+
+    const STATUS_CONSUMED = 'consumed';
+
+    /**
+     * @param int|null $date
+     * @return $this
+     */
+    public function setExpirationDate($date)
+    {
+        $this->expirationDate = $date ? (int) ($date / 1000) : null;
+        return $this;
+    }
+
+    /**
+     * Return timestamp of expiration date
+     *
+     * @return int|null
+     */
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+
+    /**
+     * @param string $status
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return (string)$this->status;
+    }
+}
